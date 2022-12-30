282b0dbfee4dde63d5d1e6149536d661edc8a923
Prappo Prince <prappo.prince@gmail.com>
2022-12-30 15:06:25 +0000

    few update

--- next.config.js
+++ next.config.js
@@ -3,10 +3,4 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.tsx',
 })
 
-module.exports = {
-  ...withNextra(),
-  images: {
-    unoptimized: true,
-  },
-  assetPrefix: './',
-};
+module.exports = withNextra()
